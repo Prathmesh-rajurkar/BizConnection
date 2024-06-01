d544373638afbe81154a74be9cdb609c6c24dd5e
Prathmesh-rajurkar <prathmeshrajurkar199@gmail.com>
2024-06-01 17:47:14 +0530

    code push

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Home from './components/Home';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <div>
       <Home/>
+      <Analytics/>
     </div>
   );
 }
