763a871986079c757bd6b4743988788906efa0d0
Prathmesh-rajurkar <prathmeshrajurkar199@gmail.com>
2024-06-21 11:51:55 +0530

    express setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,28 @@
 // require('dotenv').config({path:'./env'});
 import dotenv from "dotenv"
 import connectDB from './db/index.js';
+import { app } from "./app.js";
+
 
 dotenv.config({
     path:'./env'
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.on('error',(error)=>{
+        console.log("error",error);
+        throw error;
+       });
+
+       
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Listening on ${process.env.PORT}`);
+    })
+})
+.catch((error)=>{
+    console.log("mongoDB error: " + error);
+})
 
 
 
