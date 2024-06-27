8604efc65489eadfaa517b6bd8aa3cf898d4d1fb
Prathmesh-rajurkar <prathmeshrajurkar199@gmail.com>
2024-06-27 10:15:14 +0530

    api req send

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,7 +18,7 @@ app.use(cookieParser())
 
 import userRouter from './routes/user.routes.js';
 
-
+app.use("/api/v1/users",userRouter);
 
 
 export {app} ;
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,6 +1,8 @@
-import { Router } from './Router';
+import { registerUser } from '../controllers/user.controller.js';
+import { Router } from 'express';
 
 const router = Router()
 
+router.route("/register").post(registerUser)
 
 export default router
